ed62fe53d5ab68bf8ab3821896fa5f856d8c88ae
NullSec <nullsec0001@proton.me>
2026-04-22 12:08:44 +0530

    final

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,20 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Mock the Web Crypto API for uuid
+Object.defineProperty(global.self, 'crypto', {
+  value: {
+    getRandomValues: function(arr) {
+      for (let i = 0; i < arr.length; i++) {
+        arr[i] = Math.floor(Math.random() * 256);
+      }
+      return arr;
+    }
+  }
+});
+
+// Polyfill TextEncoder for JSDOM
+if (typeof global.TextEncoder === 'undefined') {
+  global.TextEncoder = require('util').TextEncoder;
+}
